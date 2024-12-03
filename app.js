4e94e483a7a70e609969f80547ded2195997da57
Sarahalhbaish <sarah21995@live.com>
2024-12-03 17:44:57 +0300

    Done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(name= "Sarah AlRashidi");
+  res.send({name: "Sarah AlRashidi"});
 });
 
 app.listen(8000, () => {
